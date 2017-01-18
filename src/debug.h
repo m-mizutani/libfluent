4480b8d381b388518db8c876c9ef08348edd7655
Caleb Anderson <canderson@flagshipbio.com>
2017-01-18 16:15:32 -0700

    fix debug to work on windows. removed time data

--- src/debug.h
+++ src/debug.h
@@ -32,21 +32,33 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-#define debug(X, ...)                                                    \
-  do {                                                                  \
-    struct tm td;                                                       \
-    struct timeval tv;                                                  \
-    struct timezone tz;                                                 \
-    FILE * out = stderr;                                                \
-    if (X) {                                                            \
-      gettimeofday (&tv, &tz);                                          \
-      localtime_r (&tv.tv_sec, &td);                                    \
-      fprintf (out, "(DEBUG %02d:%02d:%02d.%03d %s:%d %s) ", td.tm_hour, td.tm_min, td.tm_sec, static_cast <int> (tv.tv_usec / 1000), __FILE__, __LINE__, __FUNCTION__); \
-      fprintf (out, __VA_ARGS__);                                       \
-      fprintf (out, "\n");                                              \
-      fflush (out);                                                     \
-    }                                                                   \
-  } while (0);
-
+#ifdef _WIN32
+  #define debug(X, ...)                                                    \
+    do {                                                                  \
+      FILE * out = stdout;                                                \
+      if (X) {                                                            \
+        fprintf(out, "(DEBUG %s:%d %s)", __FILE__, __LINE__, __FUNCTION__); \
+        fprintf (out, __VA_ARGS__);                                       \
+        fprintf (out, "\n");                                              \
+        fflush (out);                                                     \
+      }                                                                   \
+    } while (0);
+#else
+  #define debug(X, ...)                                                    \
+    do {                                                                  \
+      struct tm td;                                                       \
+      struct timeval tv;                                                  \
+      struct timezone tz;                                                 \
+      FILE * out = stderr;                                                \
+      if (X) {                                                            \
+        gettimeofday (&tv, &tz);                                          \
+        localtime_r (&tv.tv_sec, &td);                                    \
+        fprintf (out, "(DEBUG %02d:%02d:%02d.%03d %s:%d %s) ", td.tm_hour, td.tm_min, td.tm_sec, static_cast <int> (tv.tv_usec / 1000), __FILE__, __LINE__, __FUNCTION__); \
+        fprintf (out, __VA_ARGS__);                                       \
+        fprintf (out, "\n");                                              \
+        fflush (out);                                                     \
+      }                                                                   \
+    } while (0);
+#endif // _WIN32
 
 #endif  // SRC_DEBUG_H__
